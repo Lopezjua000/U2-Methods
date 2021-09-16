c57c99e8cc81f7c5555493da74648e52de687c93
lopezjua000 <Lopezjua000@warren.k12.in.us>
2021-09-16 12:54:52 -0400

    Completed Circlecalc, Returnmethod, TrianglePerimeter

--- L3-Return Methods/src/CircleCalc.java
+++ L3-Return Methods/src/CircleCalc.java
@@ -1,15 +1,68 @@
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 public class CircleCalc {
+
+   /*
+        1. Get the input - radius
+        2. Calculate area
+        3. Calculate circumference
+        4. Display results - output
+    */
 
 
     public static void main(String[] args) {
 
-        //create and run the program
+        //1.
+        double radius = getInput("What is the radius?");
+        //2
+        double area = area(radius);
+        //3
+        double circumference = circumference(radius);
+        //4
+        JOptionPane.showMessageDialog(null, displayResult(area, circumference));
+
 
     }
 
     //re-write the method to calculate a circle's area using parameters and return statement
+    public static double area(double radius) {
 
+        return Math.pow(radius, 2) * Math.PI;
+
+    }
+
+    //Get the input
+    public static double getInput(String prompt) {
+        return Double.parseDouble(JOptionPane.showInputDialog(prompt));
+    }
 
     //re-write the method to calculate a circle's perimeter using parameters and return statement
+    public static double circumference(double radius) {
+
+        return 2 * Math.PI * radius;
+
+    }
+
+
+    public static String displayResult(double area, double circumference) {
+
+        DecimalFormat round = new DecimalFormat("#.00");
+
+        String message = "The area is: " + area;
+        message += " \nThe circumeference is: " + circumference;
+
+        return message;
+    }
+
+    public static void outputResults(double area, double circumference) {
+        DecimalFormat round = new DecimalFormat("#.00");
+
+        String message = "The area is: " + area;
+        message += " \nThe circumeference is: " + circumference;
+
+        JOptionPane.showMessageDialog(null, message);
+
+    }
 
 }
