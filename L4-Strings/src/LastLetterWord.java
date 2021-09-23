a2320e4d12ba2bbb1e6a174575ee62abc04ca2d1
lopezjua000 <Lopezjua000@warren.k12.in.us>
2021-09-23 13:21:13 -0400

    SplitPs and LastLetterWord

--- L4-Strings/src/LastLetterWord.java
+++ L4-Strings/src/LastLetterWord.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class LastLetterWord {
 
     /*
@@ -9,10 +11,25 @@ public class LastLetterWord {
     Ex:  Goodbye, Hello, Morning -> byelloing
     */
 
+    /*
+    1. Get the input- three words
+    2. Isolate the last three letters of ANY word
+    3. Combine the last three of three words
+    4. Output
+     */
+
     public static void main(String[] args) {
+        String word1 = JOptionPane.showInputDialog("Enter word 1: ");
+        String word2 = JOptionPane.showInputDialog("Enter word 2: ");
+        String word3 = JOptionPane.showInputDialog("Enter word 3: ");
 
-
+        JOptionPane.showMessageDialog(null, lastThree(word1) + lastThree(word2) + lastThree (word3));
 
     }
 
+    public static String lastThree(String word) {
+        return word.substring(word.length()-3);
+
+
+    }
 }
